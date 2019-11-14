5c3c9cfbc4ee77544d127e71bfc889c6ca274f2c
georgialoukatou <georgialoukatou@gmail.com>
2019-11-14 11:46:09 +0100

    reading new annotations

--- wells_prepare.sh
+++ wells_prepare.sh
@@ -8,10 +8,11 @@
 
 ######### VARIABLES
 # Variables that have been passed by the user
-RAW_FOLDER="/Users/lscpuser/Downloads/Wells/"
-output_file=$RAW_FOLDER/output.tmp
-output_file1=$RAW_FOLDER/output1.tmp
-output_file2=$RAW_FOLDER/output2.csv
+RAW_FOLDER="/Users/lscpuser/Documents/peerproject/peerproject/Wells/"
+RAW_FOLDER1="/Users/lscpuser/Documents/peerproject/peerproject/"
+output_file=${RAW_FOLDER1}output.txt
+output_file1=${RAW_FOLDER1}output1.txt
+output_file2=${RAW_FOLDER1}output2.csv
 
 
 
@@ -22,11 +23,13 @@ for corpus in $(find $RAW_FOLDER -type d);
  for chafile in $corpus/*cha;
    do
     echo $chafile
-    grep  -v  '@\|*CHI\|%mor\|%com\|%gra\|%sit\|%int\|%par\|%exp\|%act\|%xpho\|%gpx\|%eng\|%alt\|%spa\|\|\|^ .*$'  $chafile >> $output_file
+    # grep  -v  '*CHI\|%mor\|%com\|%gra\|%sit\|%int\|%par\|%exp\|%act\|%xpho\|%gpx\|%eng\|%alt\|%spa\|\|\|^ .*$'  $chafile >> ${RAW_FOLDER1}output_file
+     grep  -v  '*CHI\|%\|^@\|\|\|^ .*$'  $chafile >> $output_file
+
    done
 done
 
 
 
-sed -e 's/&[^ ]* //g'  -e 's/\[.*\]//g' -e 's/[()<>]//g' -e 's/[0-0]//g' -e "s/[^a-zA-Z' 	*%]//g" -e '/^[	| ]*$/d' -e 's/  / /g'  -e 's/	/ /g' $output_file > $output_file1
-cat $output_file1 | tr -d '\n'| tr '\*' '\n' | sed  's/%/;%/' | sed 's/ /;/'  > $output_file2
+sed -e 's/&[^ ]* //g'  -e 's/\[.*\]//g' -e 's/[()<>]//g' -e 's/[0-0]//g' -e "s/[^a-zA-Z' 	@*%]//g" -e '/^[	| ]*$/d' -e 's/  / /g'  -e 's/	/ /g' $output_file > $output_file1
+cat $output_file1 | tr -d '\n'| tr '\*' '\n' | sed  's/@/;/' | sed 's/ /;/'  > $output_file2
