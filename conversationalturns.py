6dae20ce28722dc6e7750aee378d6f28600fa1e6
georgialoukatou <georgialoukatou@gmail.com>
2020-06-23 18:44:37 +0200

    sessions

--- conversationalturns.py
+++ conversationalturns.py
@@ -3,31 +3,33 @@ import linecache
 
 #remember to check order of utterances before
 
-file1=open("/Users/admin/Documents/peerspeech_03.06.2020/frenchturns.txt","w") 
+file1=open("/Users/admin/Documents/peerspeech_13.06.2020/demuthturns.txt","w") 
 
-with open('/Users/admin/Documents/peerspeech_03.06.2020/french.txt') as fp:
+with open('/Users/admin/Documents/peerspeech_13.06.2020/demuth.txt') as fp:
   lines = fp.readlines()
   for line in range(len(lines) - 1):
        info=str(lines[line]).lower().rstrip().replace('other child', 'child').replace('target_child', 'keychild').split(';;')
-       info2= str(lines[line+1]).lower().rstrip().replace('other child', 'child').replace('target_child', 'keychild').split(';;')  
+       info2= str(lines[line+1]).lower().rstrip().replace('other child', 'child').replace('target_child', 'keychild').split(';;') 
+       print(info, info2) 
        try: 
-        speaker, addressee, register_, keychild, session_ =info[14], info[15],info[16], info[13],info[7]  
-        speaker2, addressee2, register_2, keychild2, session_2 =info2[14], info2[15],info2[16], info2[13],info2[7] 
-    #    speaker, addressee, register_, keychild, session_ =info[41], info[42],info[43], info[40],info[2] 
-     #   speaker2, addressee2, register_2, keychild2, session_2 =info2[41], info2[42],info2[43], info2[40],info2[2]
-       except:
-        print("info not found")    
-       if (str(session_) == str(session_2)  and "keychild" not in speaker and "investigator" not in speaker and "NA" not in speaker and "investigator" not in speaker2):
+  #      speaker, addressee, register_, keychild, session_ =info[14], info[15],info[16], info[13],info[7]  
+   #     speaker2, addressee2, register_2, keychild2, session_2 =info2[14], info2[15],info2[16], info2[13],info2[7] 
+        speaker, addressee, register_, keychild, session_ =info[42], info[42],info[45], info[41],info[43] 
+        speaker2, addressee2, register_2, keychild2, session_2 =info2[42], info2[42],info2[45], info2[41],info2[43]   
+        if (str(session_) == str(session_2)  and "keychild" not in speaker and "investigator" not in speaker and "investigator" not in register_ and "NA" not in speaker and "investigator" not in speaker2):
+         print(speaker, addressee, speaker2)
          if addressee == speaker2 :
           listinfo=[speaker, session_, register_, "_match"]
+         # print(listinfo)
           listinfo=str(listinfo).replace('\n', '').replace('"','').replace('/','_')
           file1.write(str(listinfo))
          else:
           listinfo=[speaker, session_, register_, "_nomatch"]
           listinfo=str(listinfo).replace('\n', '').replace('"','').replace('/','_')
           file1.write(str(listinfo))
+          print(listinfo)
+       except:
+        print("info not found") 
+
+          
 file1.close()
-
-         
-         
-
--- textanalysis.R
+++ textanalysis.R
@@ -1,4 +1,4 @@
-#Georgia Loukatou
+#georgia
 
 library(ggplot2)
 library(gridtext)
@@ -6,28 +6,30 @@ library(grid)
 library(gridExtra)
 library(gtable)
 
-demuthlongstats<-read.csv(file="/Users/admin/Documents/peerspeech_03.06.2020/shortoutputSesotho.csv", header=TRUE)
+demuthlongstats<-read.csv(file="/Users/admin/Documents/peerspeech_13.06.2020/shortoutputSesotho.csv", header=TRUE)
 demuthlongstats$language<-"Sesotho"
+demuthlongstats<-demuthlongstats[!is.na(demuthlongstats$numberoflines), ]
 
-frenchlongstats<-read.csv(file="/Users/admin/Documents/peerspeech_03.06.2020/shortoutputFrench.csv", header=TRUE) 
+frenchlongstats<-read.csv(file="/Users/admin/Documents/peerspeech_03.06.2020/dissertationDraftResults/shortoutputFrench.csv", header=TRUE) 
 frenchlongstats$language<-"French"
+frenchlongstats<-frenchlongstats[!is.na(frenchlongstats$numberoflines), ]
 
 stats<-rbind(demuthlongstats, frenchlongstats)
 
-stats<-stats[!is.na(stats$numberoflines), ]
+#stats<-stats[!is.na(stats$numberoflines), ]
 stats$ratiohapaxes<-stats$numberofhapaxes/stats$numberofwords
 stats$ratiouttsinglewords<-stats$numberofuttsinglewords/stats$numberoflines
 stats$language[grep("Hlobohang|Litlhare|Tsebo", stats$targetchild)]  <- "Sesotho"
 stats$language[grep("Anais|Anae|Theotime", stats$targetchild)]  <- "French"
 
-#across sessions - long version
+
 averagemlu<-aggregate(stats$mlu, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(averagemlu)<-c("keychild", "register", "speaker", "averagemlu")
 averagesinglewordutts<-aggregate(stats$ratiouttsinglewords, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(averagesinglewordutts)<-c("keychild", "register", "speaker", "averagesinglewordutts")
 morphosynt<-merge(x=averagemlu, y=averagesinglewordutts, by.x=c("keychild", "register", "speaker"), by.y=c("keychild", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(morphosynt)<- c("keychild", "register", "speaker", "mlumean", "singleworduttmean")
-                                 
+
 #across children - short version
 acrossmlu<-aggregate(stats$mlu, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(acrossmlu)<-c("language", "register", "speaker", "averagemlu")
@@ -35,13 +37,14 @@ acrosssinglewordutts<-aggregate(stats$ratiouttsinglewords, by=list(stats$languag
 names(acrosssinglewordutts)<-c("language", "register", "speaker", "averagesinglewordutts")
 acrossmorphosynt<-merge(x=acrossmlu, y=acrosssinglewordutts, by.x=c("language", "register", "speaker"), by.y=c("language", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(acrossmorphosynt)<- c("language", "register", "speaker", "mlumean", "singleworduttmean")
-                                 
+##
 
 #####TABLE-MORPHOSYNTAX
-write.table(morphosynt, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/morphosyntax.txt"), row.names = FALSE, col.names = TRUE)
-write.table(acrossmorphosynt, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/morphosyntax_short.txt"), row.names = FALSE, col.names = TRUE)
+write.table(morphosynt, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/morphosyntax.txt"), row.names = FALSE, col.names = TRUE)
+write.table(acrossmorphosynt, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/morphosyntax_short.txt"), row.names = FALSE, col.names = TRUE)
 
-#across sessions - long version
+acrossnumberoflines<-aggregate(stats$numberoflines, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
+
 averagequestion<-aggregate(stats$percquest, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(averagequestion)<-c("keychild", "register", "speaker", "averagequestion")
 averageconvturn<-aggregate(stats$ratioturn, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
@@ -49,23 +52,21 @@ names(averageconvturn)<-c("keychild", "register", "speaker", "averageconvturn")
 speechelicitation<-merge(x=averagequestion, y=averageconvturn, by.x=c("keychild", "register", "speaker"), by.y=c("keychild", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(speechelicitation)<- c("keychild", "register", "speaker", "questionmean", "convturnmean")
 
-#across children - short version                           
+
+
 acrossquestion<-aggregate(stats$percquest, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(acrossquestion)<-c("language", "register", "speaker", "averagequestion")
 acrossconvturn<-aggregate(stats$ratioturn, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(acrossconvturn)<-c("language", "register", "speaker", "averageconvturn")
 acrossspeechelic<-merge(x=acrossquestion, y=acrossconvturn, by.x=c("language", "register", "speaker"), by.y=c("language", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(acrossspeechelic)<- c("language", "register", "speaker", "questionratio", "convturnratio")
-                          
-                           
-                           
+
+
 #####TABLE-SPEECHELICITATION
-write.table(speechelicitation, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/speechelicitation.txt"), row.names = FALSE, col.names = TRUE)
-write.table(acrossspeechelic, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/speechelicitation_short.txt"), row.names = FALSE, col.names = TRUE)
+write.table(speechelicitation, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/speechelicitation.txt"), row.names = FALSE, col.names = TRUE)
+write.table(acrossspeechelic, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/speechelicitation_short.txt"), row.names = FALSE, col.names = TRUE)
 
 
-                          
-#across sessions - long version                          
 averagettr<-aggregate(stats$mattr, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(averagettr)<-c("keychild", "register", "speaker", "averagettr")
 averagenvratio<-aggregate(stats$nounverbratio, by=list(stats$targetchild, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
@@ -73,7 +74,6 @@ names(averagenvratio)<-c("keychild", "register", "speaker", "averagenvratio")
 lexicaldiversity<-merge(x=averagettr, y=averagenvratio, by.x=c("keychild", "register", "speaker"), by.y=c("keychild", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(lexicaldiversity)<- c("keychild", "register", "speaker", "ttrmean", "nvratiomean")
 
-#across children - short version                           
 acrossttr<-aggregate(stats$mattr, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
 names(acrossttr)<-c("language", "register", "speaker", "averagettr")
 acrossnvratio<-aggregate(stats$nounverbratio, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
@@ -81,18 +81,30 @@ names(acrossnvratio)<-c("language", "register", "speaker", "averagenvratio")
 acrosslexicaldiv<-merge(x=acrossttr, y=acrossnvratio, by.x=c("language", "register", "speaker"), by.y=c("language", "register", "speaker"), all.x=TRUE, sort=TRUE)
 colnames(acrosslexicaldiv)<- c("language", "register", "speaker", "mattr", "nounverbratio")
 
-                          
-                          
-                          
+acrossttr<-aggregate(stats$mattr, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
+names(acrossttr)<-c("language", "register", "speaker", "averagettr")
+acrosshapaxratio<-aggregate(stats$ratiohapaxes, by=list(stats$language, stats$addressee, stats$speaker), function(x) cbind(round(mean(x),2), round(sd(x),2)) )
+names(acrosshapaxratio)<-c("language", "register", "speaker", "averagehapaxratio")
+acrosslexicaldiv<-merge(x=acrossttr, y=acrosshapaxratio, by.x=c("language", "register", "speaker"), by.y=c("language", "register", "speaker"), all.x=TRUE, sort=TRUE)
+colnames(acrosslexicaldiv)<- c("language", "register", "speaker", "mattr", "hapaxratio")
+
+
+
 #####TABLE-LEXICALDIVERSITY
-write.table(lexicaldiversity, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/lexicaldiversity.txt"), row.names = FALSE, col.names = TRUE)
-write.table(acrosslexicaldiv, file=paste0("/Users/admin/Documents/peerspeech_03.06.2020/lexicaldiversity_short.txt"), row.names = FALSE, col.names = TRUE)
+write.table(lexicaldiversity, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/lexicaldiversity.txt"), row.names = FALSE, col.names = TRUE)
+write.table(acrosslexicaldiv, file=paste0("/Users/admin/Documents/peerspeech_13.06.2020/lexicaldiversity_hapax_short.txt"), row.names = FALSE, col.names = TRUE)
+
+
+#### average number of utts per session
+a<-aggregate(stats$numberoflines, by=list(stats$targetchild, stats$session), function(x) (round(sum(x),2)))
+b<-aggregate(a$x, by=list(a$Group.1), function(x) (round(mean(x),2)))
 
 
 
-                          
-                          
-####figure                           
+
+
+
+##############figures
 get_legend<-function(myggplot){
   tmp <- ggplot_gtable(ggplot_build(myggplot))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
@@ -101,9 +113,9 @@ get_legend<-function(myggplot){
 }
 
 statsna$mlu<-as.numeric(stats$mlu)
-title1=textGrob("Mean utterance length", gp=gpar(fontface="bold"))
-title2=textGrob("Type token ratio", gp=gpar(fontface="bold"))
-title3=textGrob("Question ratio", gp=gpar(fontface="bold"))
+title1=textGrob("Morphosyntactic properties ", gp=gpar(fontface="bold"))
+title2=textGrob("Lexical properties", gp=gpar(fontface="bold"))
+title3=textGrob("Interactional properties", gp=gpar(fontface="bold"))
 
 
 statsCDS<-subset(stats, (stats$addressee=="CDS")) 
@@ -113,28 +125,51 @@ statsOCDS<-subset(stats, (stats$addressee=="OCDS"))
 demuth<-subset(stats, (stats$language=="Sesotho")) 
 french<-subset(stats, (stats$language=="French")) 
 
+library(forcats)
+
+demuth$addressee <- factor(demuth$addressee,levels = c("CDS", "OCDS", "ADS"))
+french$addressee <- factor(french$addressee,levels = c("CDS", "OCDS", "ADS"))
 
 
-plot1<-ggplot(demuth, aes(x=addressee, y=mlu, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge()) + labs(title="Sesotho") + ylab("Mean utt. length") + ylim(2, 8) + theme(legend.position = "none")
-plot2<-ggplot(french, aes(x=addressee, y=mlu, fill=speaker))+ geom_boxplot()+  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge()) + labs(title="French") + ylab("Mean utt. length") + ylim(2, 8)
-legend <- get_legend(plot2)
-plot2<-ggplot(french, aes(x=addressee, y=mlu, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge()) + labs(title="French") + ylab("Mean utt. length")+ ylim(2, 8) + theme(legend.position = "none")
-grid.arrange(arrangeGrob(plot1, plot2, top=title1, ncol=2), legend, nrow = 2, heights = c(10, 1))
+demuth$targetchild<-as.factor(demuth$targetchild)
+plot1<-ggplot(demuth, aes(x=addressee, y=mlu, fill=speaker))+ geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho") + ylab("Mean utt. length") + ylim(2, 7) +scale_color_manual(values=c('#999999','#E69F00', 'violet')) #+ theme(legend.position = "none")
+plot2<-ggplot(french, aes(x=addressee, y=mlu, fill=speaker))+ geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French") + ylab("Mean utt. length") + ylim(2, 7) +scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+#legend <- get_legend(plot2)
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/mlu_new1.png", width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot1, plot2, top=title1, ncol=2),  nrow = 2, heights = c(5, 1))
+dev.off()
 
-plot3<-ggplot(demuth, aes(x=addressee, y=mattr, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge())+ labs(title="Sesotho") + ylab("Type token ratio")
-plot4<-ggplot(french, aes(x=addressee, y=mattr, fill=speaker))+ geom_boxplot() + geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge()) + labs(title="French")  + ylab("type token ratio") + ylim(0.6, 1)  + theme(legend.position = "none")
-legend <- get_legend(plot3)
-plot3<-ggplot(demuth, aes(x=addressee, y=mattr, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge())+ labs(title="Sesotho") + ylab("Type token ratio") + ylim(0.6, 1)  + theme(legend.position = "none")
-grid.arrange(arrangeGrob(plot3, plot4, top=title2, ncol=2), legend, nrow = 2, heights = c(10, 1))
+plot3<-ggplot(demuth, aes(x=addressee, y=mattr, fill=speaker)) + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho") + ylab("Type token ratio") + ylim(0.65, 1)+scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+plot4<-ggplot(french, aes(x=addressee, y=mattr, fill=speaker)) + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French")  + ylab("Type token ratio") + ylim(0.65, 1) +scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/ttr_new.png",  width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot3, plot4, top=title2, ncol=2), nrow = 2, heights = c(5, 1))
+dev.off()
 
+frenchlongstats$addressee <- factor(frenchlongstats$addressee,levels = c("CDS", "OCDS", "ADS"))
 
-plot5<-ggplot(demuth, aes(x=addressee, y=percquest, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge())+ labs(title="Sesotho") + ylab("Question ratio")
-plot6<-ggplot(french, aes(x=addressee, y=percquest, fill=speaker))+ geom_boxplot() + geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge()) + labs(title="French")  + ylab("Question ratio")  + theme(legend.position = "none")
-legend <- get_legend(plot5)
-plot5<-ggplot(demuth, aes(x=addressee, y=percquest, fill=speaker))+ geom_boxplot() +  geom_point(aes(fill = speaker), size = 0.4, shape = 21, position = position_jitterdodge())+ labs(title="Sesotho") + ylab("Question ratio") + theme(legend.position = "none")
-grid.arrange(arrangeGrob(plot5, plot6, top=title3, ncol=2), legend, nrow = 2, heights = c(10, 1))
+plot5<-ggplot(demuth, aes(x=addressee, y=percquest, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho")+ ylim(0.0, 0.9)  + ylab("Question ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+plot6<-ggplot(frenchlongstats, aes(x=addressee, y=percquest, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French") + ylim(0.0, 0.9)  + ylab("Question ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))  
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/questionratio_new.png",  width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot5, plot6, top=title3, ncol=2), nrow = 2, heights = c(5, 1))
+dev.off()
 
+plot7<-ggplot(demuth, aes(x=addressee, y=ratiouttsinglewords, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho") + ylim(0.0, 0.75) + ylab("Single word utt. ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+plot8<-ggplot(french, aes(x=addressee, y=ratiouttsinglewords, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French") + ylim(0.0, 0.75) + ylab("Single word utt. ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))  
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/singlewordutt_new.png",  width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot7, plot8, top=title1, ncol=2), nrow = 2, heights = c(5, 1))
+dev.off()
 
+plot9<-ggplot(demuth, aes(x=addressee, y=ratioturn, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho")+ ylim(0.0, 1.0) + ylab("Conv. turn ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+plot10<-ggplot(french, aes(x=addressee, y=ratioturn, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French")+ ylim(0.0, 1.0)   + ylab("Conv. turn ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))  
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/convturn_new.png",  width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot9, plot10, top=title3, ncol=2), nrow = 2, heights = c(5, 1))
+dev.off()
+
+plot11<-ggplot(demuth, aes(x=addressee, y=ratiohapaxes, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="Sesotho") + ylab("Hapax ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))
+plot12<-ggplot(french, aes(x=addressee, y=ratiohapaxes, fill=speaker))+ geom_boxplot(aes(fill=speaker))  + geom_boxplot(aes(fill=speaker)) +  geom_point(aes(fill=speaker, colour=targetchild), size = 0.6, position = position_jitterdodge()) + labs(title="French")  + ylab("Hapax ratio")+scale_color_manual(values=c('#999999','#E69F00', 'violet'))  
+png(filename="/Users/admin/Documents/peerspeech_13.06.2020/hapax_new.png",  width = 13, height = 4, units = 'in', res = 500)
+grid.arrange(arrangeGrob(plot11, plot12, top=title2, ncol=2), nrow = 2, heights = c(5, 1))
+dev.off()
 
 
 
